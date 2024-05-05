53ceb69bb6de6ea6968a10b35cb0edb660f9a854
Lucia <125606190+luciariw22@users.noreply.github.com>
2024-05-06 11:03:03 +1200

    Add files via upload

--- z_sandpit_2.py
+++ z_sandpit_2.py
@@ -1,82 +1,15 @@
-def yes_no(question):
-    to_check = ["yes", "no"]
-
-    valid = False
-    while not valid:
-
-        response = input(question).lower()
-
-        for var_item in to_check:
-            if response == var_item:
-                return response
-            elif response == var_item[0]:
-                return var_item
-
-        print("Please enter either yes or no...\n")
-
-
-def measurement():
-    error = "Please enter a valid measurement\n"
-    valid = False
-
-    while not valid:
-        response = input("\nAmount of ingredient?: (e.g., 2kg, 200g, 20mL, or "
-                         "enter number with no unit): ").strip().lower()
-        if response == "xxx":
-            return None
-
-        if response.endswith('kg'):
-            print("Amount:", response)
-            unit_type = "kg"
-
-        elif response.endswith('g'):
-            # Check if the substring before 'g' is numeric
-            if response[:-1].isdigit():
-                print("Amount:", response)
-                unit_type = "g"
-            else:
-                print(error)
-                continue
-
-        elif response.endswith('ml'):
-            print("Amount:", response)
-            unit_type = "ml"
-
-        else:
-            unit_type = response
-
-        try:
-            if unit_type == "g":
-                amount = float(response[:-1])
-            elif unit_type == "kg" or unit_type == "ml":
-                amount = float(response[:-2])
-            else:
-                amount = float(response)
-
-            if amount <= 0:
-                print("Please enter a valid amount")
-                continue
-            else:
-                valid = True
-        except ValueError:
-            print(error)
-            continue
-
-        if unit_type == response:
-            # If no unit is entered, return the amount directly
-            return amount
-
-        if unit_type == "g" or unit_type == "kg" or unit_type == "ml":
-            return amount
-
-
-# Main routine goes here
-
 while True:
-    ingredient_amount = measurement()
-    if ingredient_amount is None:
+    ingredient_name = not_blank("\nIngredient: (or enter 'xxx' to when done) ", "The ingredient name can't be blank ")
+    if ingredient_name == "xxx":
         break
-    print("Ingredient amount:", ingredient_amount)
+    packaged_amount = measurement("\nAmount ingredient is bought in?: (e.g., 2kg, 200g, 20mL, or "
+                                  "enter number with no unit): ")
 
+    print("Packaged amount:", packaged_amount)
+
+    ingredient_amount = measurement("\nAmount of ingredient needed for recipe?: (e.g., 2kg, 200g, 20mL, or "
+                                    "enter number with no unit): ")
+
+    print("Ingredient amount:", ingredient_amount)
 
 
