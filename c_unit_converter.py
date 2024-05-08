cd388d3fb59a2edcf3ebc5a4a8042492c922f618
Lucia <125606190+luciariw22@users.noreply.github.com>
2024-05-08 13:24:48 +1200

    Add files via upload

--- c_unit_converter.py
+++ c_unit_converter.py
@@ -1,16 +1,89 @@
-def kg_to_grams(kg):
-    grams = kg * 1000
-    return grams
+def yes_no(question):
+    to_check = ["yes", "no"]
+
+    valid = False
+    while not valid:
+
+        response = input(question).lower()
+
+        for var_item in to_check:
+            if response == var_item:
+                return response
+            elif response == var_item[0]:
+                return var_item
+
+        print("Please enter either yes or no...\n")
 
 
-def main():
-    try:
-        kg = float(input("Enter weight in kilograms: "))
-        grams = kg_to_grams(kg)
-        print(f"{kg} kilograms ({grams} grams.)")
-    except ValueError:
-        print("Please enter a valid number.")
+def measurement(question):
+    error = "Please enter a valid measurement\n"
+    valid = False
+
+    while not valid:
+        response = input(question).lower()
+        if response == "xxx":
+            return None
+
+        if response.endswith('kg'):
+            print("Amount:", response)
+            unit_type = "kg"
+
+        elif response.endswith('g'):
+            # Check if the substring before 'g' is numeric
+            if response[:-1].isdigit():
+                print("Amount:", response)
+                unit_type = "g"
+            else:
+                print(error)
+                continue
+
+        elif response.endswith('ml'):
+            print("Amount:", response)
+            unit_type = "ml"
+
+        else:
+            unit_type = response
+
+        try:
+            if unit_type == "g":
+                amount = float(response[:-1])
+            elif unit_type == "kg" or unit_type == "ml":
+                amount = float(response[:-2])
+            else:
+                amount = float(response)
+
+            if amount <= 0:
+                print("Please enter a valid amount")
+                continue
+            else:
+                valid = True
+        except ValueError:
+            print(error)
+            continue
+
+        if unit_type == response:
+            # If no unit is entered, return the amount directly
+            return amount
+
+        if unit_type == "g" or unit_type == "kg" or unit_type == "ml":
+            return amount
 
 
-if __name__ == "__main__":
-    main()
+def unit_converter():
+    valid = False
+
+    while not valid:
+        if unit_type = "kg"
+        converted_amount = amount * 1000
+
+
+# Main routine goes here
+
+while True:
+    ingredient_amount = measurement("\nAmount of ingredient?: (e.g., 2kg, 200g, 20mL, or "
+                                    "enter number with no unit): ")
+    if ingredient_amount is None:
+        break
+    print("Ingredient amount:", ingredient_amount)
+
+    print(converted_amount)
