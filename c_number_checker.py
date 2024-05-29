431a9ef134f4e59519617b8a040b42b97acb684b
Lucia <125606190+luciariw22@users.noreply.github.com>
2024-05-30 10:37:25 +1200

    Add files via upload

--- c_number_checker.py
+++ c_number_checker.py
@@ -1,37 +1,29 @@
-# import libraries
-
-
-# functions go here
-
-# checks that input is either a float or an
-# integer that is more than zero. Takes in custom error message
+# Function to check if input is a positive integer
 def num_check(question, error, num_type):
     valid = False
 
     while not valid:
-
         try:
             response = num_type(input(question))
 
             if response <= 0:
                 print(error)
-
             else:
                 return response
-
         except ValueError:
             print(error)
 
 
-# Main routine goes here
+# Main routine
+while True:
+    serving_amount = num_check("Serving amount?: ",
+                               "The serving size must be a whole integer higher than 0. \n",
+                               int)
+    print("Program Continues")
+    print()
 
-get_int = num_check("How many do you need? ",
-                    "Please enter an amount more than 0\n",
-                    int)
-get_cost = num_check("How much does it cost? $",
-                     "Please enter a number more than 0\n",
-                     float)
 
-print("You need: {}".format(get_int))
-print("It costs: ${}".format(get_cost))
+
+
+
 
