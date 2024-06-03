7f1436e6611a15e1fbd510617465ea911f6875c0
Lucia <125606190+luciariw22@users.noreply.github.com>
2024-06-04 11:50:45 +1200

    Add files via upload

--- B_recipe_cost_calculator.py
+++ B_recipe_cost_calculator.py
@@ -1,11 +1,29 @@
-# Functions go here
+import pandas
 
 
-# checks input is a float or integer
+# prints instructions
 def instructions():
     print()
     print("instructions go here...")
     print()
+
+
+# yes / no checker
+def yes_no(question):
+    to_check = ["yes", "no"]
+
+    valid = False
+    while not valid:
+
+        response = input(question).lower()
+
+        for var_item in to_check:
+            if response == var_item:
+                return response
+            elif response == var_item[0]:
+                return var_item
+
+        print("Please enter either yes or no...\n")
 
 
 # number checker
@@ -27,25 +45,7 @@ def num_check(question, error, num_type):
             print(error)
 
 
-# yes / no checker
-def yes_no(question):
-    to_check = ["yes", "no"]
-
-    valid = False
-    while not valid:
-
-        response = input(question).lower()
-
-        for var_item in to_check:
-            if response == var_item:
-                return response
-            elif response == var_item[0]:
-                return var_item
-
-        print("Please enter either yes or no...\n")
-
-
-# Checks input isn't blank
+# ensures inputs can't be blank
 def not_blank(question, error):
     valid = False
     while not valid:
@@ -58,8 +58,8 @@ def not_blank(question, error):
         return response
 
 
-# gets ingredient amounts and measurements
-def measurement(question):
+# gets unit of amounts
+def measurement(question, error):
     error = "Please enter a valid measurement\n"
     valid = False
 
@@ -73,7 +73,6 @@ def measurement(question):
             unit_type = "kg"
 
         elif response.endswith('g'):
-            # Check if the substring before 'g' is numeric
             if response[:-1].isdigit():
                 print("Amount:", response)
                 unit_type = "g"
@@ -106,14 +105,103 @@ def measurement(question):
             continue
 
         if unit_type == response:
-            # If no unit is entered, return the amount directly
-            return amount
+            print("Amount:", response)
 
-        if unit_type == "g" or unit_type == "kg" or unit_type == "ml":
-            return amount
+            return amount, None
+
+        return amount, unit_type
 
 
-# ====== Main routine goes here ======
+# currency formatting function
+def currency(x):
+    return "${:.2f}".format(x)
+
+
+# gets costs and puts them into lists
+def get_ingredient_costs():
+    # Set up dictionaries and lists
+    ingredient_list = []
+    store_amount_list = []
+    amount_list = []
+    price_list = []
+    cost_list = []  # Add a new list to store the cost of each ingredient
+
+    variable_dict = {
+        "Ingredient": ingredient_list,
+        "Store Amount": store_amount_list,
+        "Recipe Amount": amount_list,
+        "Price": price_list,
+        "Cost": cost_list
+    }
+
+    # loop to get ingredient, amount, and price
+    ingredient_name = ""
+    while ingredient_name.lower() != "xxx":
+
+        print()
+        # get name, store amount, amount, and price
+        ingredient_name = not_blank("Ingredient name: ",
+                                    "The ingredient name can't be "
+                                    "blank.")
+        if ingredient_name.lower() == "xxx":
+            break
+
+        store_amount, store_unit = measurement("Amount ingredient is bought in from store?: ",
+                                               "Please enter a valid measurement")
+        store_amount_list.append(store_amount)
+        if store_unit == "kg":
+            store_converted_amount = store_amount * 1000
+        elif store_unit == "ml":
+            store_converted_amount = store_amount
+        else:
+            store_converted_amount = store_amount
+
+        print("Converted store amount:", store_converted_amount, "g" if store_unit is not None else "")
+
+        amount, unit = measurement("Recipe Amount: ", "Please enter a valid measurement")
+        amount_list.append(amount)
+
+        if unit == "kg":
+            converted_amount = amount * 1000
+        elif unit == "ml":
+            converted_amount = amount
+        else:
+            converted_amount = amount
+
+        print("Converted recipe amount:", converted_amount, "g" if unit is not None else "")
+
+        price = num_check("How much for a single item? $",
+                          "The price must be a number <more "
+                          "than 0>",
+                          float)
+        price_list.append(price)
+
+        # Calculate cost for the current ingredient
+        cost = price / store_converted_amount * converted_amount
+        cost_list.append(cost)
+
+        # add item to lists
+        ingredient_list.append(ingredient_name)
+
+    expense_frame = pandas.DataFrame(variable_dict)
+    expense_frame = expense_frame.set_index('Ingredient')
+
+    # Find sub-total
+    sub_total = expense_frame['Cost'].sum()
+
+    # Calculate cost per serving
+    expense_frame['Total Cost'] = sub_total
+    expense_frame['Cost per serving'] = sub_total / serving_size
+
+    # Currency formatting
+    add_dollars = ['Price', 'Cost']
+    for item in add_dollars:
+        expense_frame[item] = expense_frame[item].apply(currency)
+
+    return [expense_frame, sub_total]
+
+
+# *** Main Routine starts here ***
 
 # Ask user if they want to see the instructions
 want_instructions = yes_no("Do you want to see the instructions?")
@@ -121,20 +209,29 @@ want_instructions = yes_no("Do you want to see the instructions?")
 if want_instructions == "yes":
     instructions()
 
-product_name = not_blank("Recipe name: ", "The product name can't be blank. ")
-serving_size = not_blank, num_check("Serving Size: ", "The serving size can't be blank "
-                                                      "and must be an integer", int)
+# Get product name
+recipe_name = not_blank("Recipe name: ", "The Recipe name can't be blank.")
+serving_size = num_check("Serving size: ",
+                         "The serving size can't be blank and must be a whole integer higher than 0. ", int)
 
-while True:
-    ingredient_name = not_blank("\nIngredient: (or enter 'xxx' to when done) ", "The ingredient name can't be blank ")
-    if ingredient_name == "xxx":
-        break
-    packaged_amount = measurement("\nAmount ingredient is bought in?: (e.g., 2kg, 200g, 20mL, or "
-                                  "enter number with no unit): ")
+variable_expenses = get_ingredient_costs()
+variable_frame = variable_expenses[0]
+variable_sub = variable_expenses[1]
 
-    print("Packaged amount:", packaged_amount)
+# *** Printing Area ***
 
-    ingredient_amount = measurement("\nAmount of ingredient needed for recipe?: (e.g., 2kg, 200g, 20mL, or "
-                                    "enter number with no unit): ")
+print()
+recipe_heading = "***** -- {} Recipe -- serves {} -- *****".format(recipe_name, serving_size)
+print(recipe_heading)
+print()
+print(
+    variable_frame.drop(columns=['Cost per serving', 'Total Cost']))  # Remove Cost per serving columns from main frame
+print()
 
-    print("Ingredient amount:", ingredient_amount)
+cost_per_serv_heading = "**** Cost per Serving ****"
+print(cost_per_serv_heading)
+print()
+print("Total cost: ${:.2f}".format(variable_sub))
+print()
+print("Cost per serving: ${:.2f}".format(variable_sub / serving_size))
+print()
