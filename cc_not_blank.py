a1a250fd85948f57bad23f21f9fc6a4a035189b7
Lucia <125606190+luciariw22@users.noreply.github.com>
2024-06-04 12:21:33 +1200

    Add files via upload

--- cc_not_blank.py
+++ cc_not_blank.py
@@ -1,18 +1,18 @@
 # functions go here
 # checks that user's name is not blank
 def not_blank(question):
-
     while True:
         response = input(question)
 
         if response == "":
-            print("Sorry this can't be blank. Please try again")
+            print("This can't be blank. Please try again")
         else:
             return response
 
+
 # main routine goes here
 while True:
-    name = not_blank("enter your name (or 'xxx' to quit) ")
+    name = not_blank("\nEnter recipe name: (or 'xxx' to quit) ")
     if name == "xxx":
         break
 
